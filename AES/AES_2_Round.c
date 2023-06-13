1b784fbb1bb043db191c1a984b9a91043b50fbb7
ayush <ayush@example.com>
2023-06-13 19:56:18 +0530

    dv

---
+++ AES/AES_2_Round.c
@@ -0,0 +1,195 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int statemt[16];
+
+int word[4][120] = {{43,40,171,9,160,136,35,42,242,122,89,115,61,71,30,109,239,168,182,219,212,124,202,17,109,17,219,202,78,95,132,78,234,181,49,127,172,25,40,87,208,201,225,182,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+                   ,{126,174,247,207,250,84,163,108,194,150,53,89,128,22,35,122,68,82,113,11,209,131,242,249,136,11,249,0,84,95,166,166,210,141,43,141,119,250,209,92,20,238,63,99,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+                   ,{21,210,21,79,254,44,57,118,149,185,128,246,71,254,126,136,165,91,37,173,198,157,184,21,163,62,134,147,247,201,79,220,115,186,245,41,102,220,41,0,249,37,12,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+                   ,{22,166,136,60,23,177,57,5,242,67,122,127,125,62,68,59,65,127,59,0,248,135,188,188,122,253,65,253,14,243,178,79,33,210,96,47,243,33,65,110,168,137,200,166,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+    
+int Sbox[16][16] = {
+        {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b,
+        0xfe, 0xd7, 0xab, 0x76},
+        {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf,
+        0x9c, 0xa4, 0x72, 0xc0},
+        {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1,
+        0x71, 0xd8, 0x31, 0x15},
+        {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2,
+        0xeb, 0x27, 0xb2, 0x75},
+        {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3,
+        0x29, 0xe3, 0x2f, 0x84},
+        {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39,
+        0x4a, 0x4c, 0x58, 0xcf},
+        {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f,
+        0x50, 0x3c, 0x9f, 0xa8},
+        {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21,
+        0x10, 0xff, 0xf3, 0xd2},
+        {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d,
+        0x64, 0x5d, 0x19, 0x73},
+        {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14,
+        0xde, 0x5e, 0x0b, 0xdb},
+        {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62,
+        0x91, 0x95, 0xe4, 0x79},
+        {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea,
+        0x65, 0x7a, 0xae, 0x08},
+        {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f,
+        0x4b, 0xbd, 0x8b, 0x8a},
+        {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9,
+        0x86, 0xc1, 0x1d, 0x9e},
+        {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9,
+        0xce, 0x55, 0x28, 0xdf},
+        {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f,
+        0xb0, 0x54, 0xbb, 0x16}
+    };
+
+int ret[4 * 4];
+
+void AddRoundKey (int n)
+{
+  int j;
+  int nb = 4;
+  for (j = 0; j < nb; ++j)
+    {
+      statemt[j * 4] ^= word[0][j + nb * n];
+      statemt[1 + j * 4] ^= word[1][j + nb * n];
+      statemt[2 + j * 4] ^= word[2][j + nb * n];
+      statemt[3 + j * 4] ^= word[3][j + nb * n];
+    }
+}
+
+
+void ByteSub_ShiftRow()
+{
+  int temp;
+  temp = Sbox[statemt[1] >> 4][statemt[1] & 0xf];
+  statemt[1] = Sbox[statemt[5] >> 4][statemt[5] & 0xf];
+  statemt[5] = Sbox[statemt[9] >> 4][statemt[9] & 0xf];
+  statemt[9] = Sbox[statemt[13] >> 4][statemt[13] & 0xf];
+  statemt[13] = temp;
+
+  temp = Sbox[statemt[2] >> 4][statemt[2] & 0xf];
+  statemt[2] = Sbox[statemt[10] >> 4][statemt[10] & 0xf];
+  statemt[10] = temp;
+  temp = Sbox[statemt[6] >> 4][statemt[6] & 0xf];
+  statemt[6] = Sbox[statemt[14] >> 4][statemt[14] & 0xf];
+  statemt[14] = temp;
+
+  temp = Sbox[statemt[3] >> 4][statemt[3] & 0xf];
+  statemt[3] = Sbox[statemt[15] >> 4][statemt[15] & 0xf];
+  statemt[15] = Sbox[statemt[11] >> 4][statemt[11] & 0xf];
+  statemt[11] = Sbox[statemt[7] >> 4][statemt[7] & 0xf];
+  statemt[7] = temp;
+
+  statemt[0] = Sbox[statemt[0] >> 4][statemt[0] & 0xf];
+  statemt[4] = Sbox[statemt[4] >> 4][statemt[4] & 0xf];
+  statemt[8] = Sbox[statemt[8] >> 4][statemt[8] & 0xf];
+  statemt[12] = Sbox[statemt[12] >> 4][statemt[12] & 0xf];
+}
+
+void MixColumn_AddRoundKey (int nb, int n)
+{
+  int j;
+  int x;
+
+  for (j = 0; j < nb; ++j)
+    {
+      ret[j * 4] = (statemt[j * 4] << 1);
+      if ((ret[j * 4] >> 8) == 1)
+	      ret[j * 4] ^= 283;
+      x = statemt[1 + j * 4];
+      x ^= (x << 1);
+      if ((x >> 8) == 1)
+	      ret[j * 4] ^= (x ^ 283);
+      else
+	      ret[j * 4] ^= x;
+      ret[j * 4] ^=statemt[2 + j * 4] ^ statemt[3 + j * 4] ^ word[0][j + nb * n];
+      ret[1 + j * 4] = (statemt[1 + j * 4] << 1);
+      if ((ret[1 + j * 4] >> 8) == 1)
+	      ret[1 + j * 4] ^= 283;
+      x = statemt[2 + j * 4];
+      x ^= (x << 1);
+      if ((x >> 8) == 1)
+	      ret[1 + j * 4] ^= (x ^ 283);
+      else
+	      ret[1 + j * 4] ^= x;
+      ret[1 + j * 4] ^=statemt[3 + j * 4] ^ statemt[j * 4] ^ word[1][j + nb * n];
+
+      ret[2 + j * 4] = (statemt[2 + j * 4] << 1);
+      if ((ret[2 + j * 4] >> 8) == 1)
+	      ret[2 + j * 4] ^= 283;
+      x = statemt[3 + j * 4];
+      x ^= (x << 1);
+      if ((x >> 8) == 1)
+	      ret[2 + j * 4] ^= (x ^ 283);
+      else
+	      ret[2 + j * 4] ^= x;
+      ret[2 + j * 4] ^= statemt[j * 4] ^ statemt[1 + j * 4] ^ word[2][j + nb * n];
+
+      ret[3 + j * 4] = (statemt[3 + j * 4] << 1);
+      if ((ret[3 + j * 4] >> 8) == 1)
+	      ret[3 + j * 4] ^= 283;
+      x = statemt[j * 4];
+      x ^= (x << 1);
+      if ((x >> 8) == 1)
+	      ret[3 + j * 4] ^= (x ^ 283);
+      else
+	      ret[3 + j * 4] ^= x;
+      ret[3 + j * 4] ^= statemt[1 + j * 4] ^ statemt[2 + j * 4] ^ word[3][j + nb * n];
+    }
+
+    for (j = 0; j < nb; ++j)
+    {
+      statemt[j * 4] = ret[j * 4];
+      statemt[1 + j * 4] = ret[1 + j * 4];
+      statemt[2 + j * 4] = ret[2 + j * 4];
+      statemt[3 + j * 4] = ret[3 + j * 4];
+    }
+  
+}
+
+int main(int argc, char** argv)
+{
+      statemt[0] = atoi(argv[1]);
+      statemt[1] = atoi(argv[2]);
+      statemt[2] = atoi(argv[3]);
+      statemt[3] = atoi(argv[4]);
+      statemt[4] = atoi(argv[5]);
+      statemt[5] = atoi(argv[6]);
+      statemt[6] = atoi(argv[7]);
+      statemt[7] = atoi(argv[8]);
+      statemt[8] = atoi(argv[9]);
+      statemt[9] = atoi(argv[10]);
+      statemt[10] = atoi(argv[11]);
+      statemt[11] = atoi(argv[12]);
+      statemt[12] = atoi(argv[13]);
+      statemt[13] = atoi(argv[14]);
+      statemt[14] = atoi(argv[15]);
+      statemt[15] = atoi(argv[16]);
+
+      AddRoundKey(0);
+      for(int i=1;i<=2;i++)
+      {
+        ByteSub_ShiftRow();
+        MixColumn_AddRoundKey(4, i);
+      }
+      ByteSub_ShiftRow();
+      AddRoundKey(10);
+      printf("%d\n",statemt[0]);
+      printf("%d\n",statemt[1]);
+      printf("%d\n",statemt[2]);
+      printf("%d\n",statemt[3]);
+      printf("%d\n",statemt[4]);
+      printf("%d\n",statemt[5]);
+      printf("%d\n",statemt[6]);
+      printf("%d\n",statemt[7]);
+      printf("%d\n",statemt[8]);
+      printf("%d\n",statemt[9]);
+      printf("%d\n",statemt[10]);
+      printf("%d\n",statemt[11]);
+      printf("%d\n",statemt[12]);
+      printf("%d\n",statemt[13]);
+      printf("%d\n",statemt[14]);
+      printf("%d\n",statemt[15]);
+}
